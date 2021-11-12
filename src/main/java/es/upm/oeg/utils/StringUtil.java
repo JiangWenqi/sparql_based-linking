8a961cd8739086ccf8d70f2698e1081e4f733e7b
JiangWenqi <jiangwenqi1995@gmail.com>
2021-11-12 20:54:13 +0100

    Initial commit

---
+++ src/main/java/es/upm/oeg/utils/StringUtil.java
@@ -0,0 +1,21 @@
+package es.upm.oeg.utils;
+
+import info.debatty.java.stringsimilarity.JaroWinkler;
+
+/**
+ * @author Wenqi Jiang
+ */
+public class StringUtil {
+    private static final JaroWinkler STRING_SIMILARITY_TOOL = new JaroWinkler();
+
+
+    /**
+     *
+     * @param text1
+     * @param text2
+     * @return The similarity between two texts
+     */
+    public static double similarity(String text1, String text2) {
+        return STRING_SIMILARITY_TOOL.similarity(text1, text2);
+    }
+}
